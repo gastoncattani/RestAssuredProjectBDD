51a20aa8e30531cfe326241b6788f48ee4ee6301
gastoncattani <gastoncattani@gmail.com>
2024-07-10 12:18:48 -0300

    Running my first project in BDD!

---
+++ src/test/java/stepdefinitions/StepDefinition.java
@@ -0,0 +1,77 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class StepDefinition {
+
+    RequestSpecification request;
+    ResponseSpecification resspec;
+    Response response;
+
+    @Given("Add Place Payload")
+    public void add_place_payload() {
+        // Write code here that turns the phrase above into concrete actions
+        AddPlace p = new AddPlace();
+        p.setAccuracy(50);
+        p.setAddress("J A Frias 7777");
+        p.setLanguague("French-IN");
+        p.setPhone_number("+5491111111111");
+        p.setWebSite("www.google.com");
+        p.setName("Gaston");
+        List<String> myList = new ArrayList<>();
+        myList.add("Shoe Park");
+        myList.add("Shop");
+        p.setTypes(myList);
+        Location l = new Location();
+        l.setLat(2.3);
+        l.setLng(-3.2);
+        p.setLocation(l);
+
+        RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").
+                addQueryParam("key", "qaclick123").setContentType(ContentType.JSON).build();
+
+        resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+
+        request = given().spec(req).log().all()
+                .body(p);
+    }
+
+    @When("User calls {string} with POST http request")
+    public void user_calls_with_post_http_request(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        response = request.when().post("/maps/api/place/add/json")
+                //.then().assertThat().statusCode(200).extract().response();
+                .then().spec(resspec).extract().response();
+    }
+
+    @Then("The API call is success with status code {int}")
+    public void the_api_call_is_success_with_status_code(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Then("{string} in response body is {string}")
+    public void in_response_body_is(String keyValue, String expectedValue) {
+        // Write code here that turns the phrase above into concrete actions
+        String resp = response.asString();
+        JsonPath js = new JsonPath(resp);
+        assertEquals(js.getString(keyValue), expectedValue);
+    }
+}
